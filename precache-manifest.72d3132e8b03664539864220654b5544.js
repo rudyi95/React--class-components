dc441401a114e3a4e1b3753ab5859ab2eeffa614
rudyi95 <rud11@meta.ua>
2020-04-03 14:32:01 +0300

    Updates

---
+++ precache-manifest.72d3132e8b03664539864220654b5544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d90e4af259386ac9b5231f5241f4edd",
+    "url": "/React--class-components/index.html"
+  },
+  {
+    "revision": "4a4f2e259a96200517b5",
+    "url": "/React--class-components/static/css/main.f80b42af.chunk.css"
+  },
+  {
+    "revision": "2dec81a8e2bd96142243",
+    "url": "/React--class-components/static/js/2.650707c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React--class-components/static/js/2.650707c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4f2e259a96200517b5",
+    "url": "/React--class-components/static/js/main.207a2772.chunk.js"
+  },
+  {
+    "revision": "e947fd40a8822b71db6d",
+    "url": "/React--class-components/static/js/runtime-main.5224c7ae.js"
+  }
+]);
